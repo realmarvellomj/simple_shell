c92cf3392fc7f2c245c6aab27d089474c6db4cd2
Marvellous Olarinde <o.omarvell98@gmail.com>
2023-08-23 17:22:25 -0700

    main loop

---
+++ main_loop.c
@@ -0,0 +1,164 @@
+#include "shell.h"
+
+/**
+* hsh - main loop
+* @info: the variable & attributes
+* @av: the parameters from main()
+*
+* Return: 0 if it succeeds, 1 if it fails,
+*/
+
+int hsh(info_t *info, char **av)
+{
+	ssize_t a = 0;
+	int builtin_ret = 0;
+
+	while (a != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		a = get_input(info);
+		if (a != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
+
+/**
+* find_builtin - looks out for  a builtin command
+* @info: the attributes & variables
+*
+* Return: -1 if a  builtin command is missing,
+*			0 if builtin command is found and successful,
+*			1 if builtin command is  found but not succeeded,
+*			-2 if builtin command signifies exit()
+*/
+
+int find_builtin(info_t *info)
+{
+	int x, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
+
+	for (x = 0; builtintbl[x].type; x++)
+		if (_strcmp(info->argv[0], builtintbl[x].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[x].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
+
+/**
+* find_cmd - finding a command in the  PATH
+* @info: variable & argument
+*
+* Return: if it returns NULL
+*/
+
+void find_cmd(info_t *info)
+{
+	char *path = NULL;
+	int l, z;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (l = 0, z = 0; info->arg[l]; l++)
+		if (!is_delim(info->arg[l], " \t\n"))
+			z++;
+	if (!z)
+		return;
+
+	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_cmd(info);
+	}
+	else
+	{
+		if ((interactive(info) || _getenv(info, "PATH=")
+			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "Unavailable\n");
+		}
+	}
+}
+
+/**
+* fork_cmd - forks a new process to run the command
+* @info: variable and attributes
+*
+* Return: returns NULL
+*/
+
+void fork_cmd(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: create a put error function */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: create a put error function */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Access denied\n");
+		}
+	}
+}
